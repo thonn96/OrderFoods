77e62b80024b920e5af3388424bdfc6cc541c10a
thonn96 <thonn296@gmail.com>
2020-04-16 16:34:01 -0700

    + add function Search Food

--- app/src/main/java/com/example/orderfoods/FoodList.java
+++ app/src/main/java/com/example/orderfoods/FoodList.java
@@ -2,9 +2,14 @@ package com.example.orderfoods;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,9 +20,16 @@ import com.example.orderfoods.Model.Food;
 import com.example.orderfoods.ViewHolder.FoodViewHolder;
 import com.example.orderfoods.ViewHolder.MenuViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoodList extends AppCompatActivity {
 
@@ -28,6 +40,14 @@ public class FoodList extends AppCompatActivity {
     RecyclerView.LayoutManager layoutManager;
     String catogeryId = "";
     FirebaseRecyclerAdapter<Food, FoodViewHolder> adapter;
+
+    //function search Food
+    FirebaseRecyclerAdapter<Food, FoodViewHolder> searchAdapter;
+    List<String> suggestList = new ArrayList<>();
+    MaterialSearchBar materialSearchBar;
+
+
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +55,6 @@ public class FoodList extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance();
         foodList = database.getReference("Food");
-
         recycler_food = (RecyclerView)findViewById(R.id.recyclerview_food);
         recycler_food.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
@@ -45,6 +64,103 @@ public class FoodList extends AppCompatActivity {
         if(!catogeryId.isEmpty() && catogeryId != null){
             loadListFood(catogeryId);
         }
+        //Search
+
+        materialSearchBar = (MaterialSearchBar)findViewById(R.id.searchBar);
+        materialSearchBar.setText("Enter your Food");
+      //  materialSearchBar.setSpeechMode(false);
+        loadSuggest();
+        materialSearchBar.setLastSuggestions(suggestList);
+        materialSearchBar.setCardViewElevation(10);
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                List<String> suggest = new ArrayList<String>();
+                for(String search:suggestList){
+                    if(search.toLowerCase().contains(materialSearchBar.getText().toLowerCase()))
+                        suggest.add(search);
+                }
+                materialSearchBar.setLastSuggestions(suggest);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                //when search Bar is Close
+                // Restore ogirinal suggest adapter
+                if(!enabled)
+                    recycler_food.setAdapter(adapter);
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                startSearch(text);
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+
+    }
+
+    private void startSearch(CharSequence text) {
+        searchAdapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(
+                Food.class,
+                R.layout.menu_item,
+                FoodViewHolder.class,
+                foodList.orderByChild("Name").equalTo(text.toString())
+        ) {
+            @Override
+            protected void populateViewHolder(FoodViewHolder foodViewHolder, Food food, int i) {
+                foodViewHolder.txt_food_name.setText(food.getName());
+                Picasso.get().load(food.getImage()).into(foodViewHolder.img_food_image);
+
+               final Food local = food;
+
+                foodViewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+
+                        //start Activity
+                        Intent foodDetail = new Intent(FoodList.this,FoodDetal.class);
+                        foodDetail.putExtra("FoodId",searchAdapter.getRef(position).getKey());
+                        startActivity(foodDetail);
+                    }
+                });
+             }
+         };
+        recycler_food.setAdapter(searchAdapter);// set Adapter for Recycel View is Search result;
+
+    }
+
+
+    private void loadSuggest() {
+        foodList.orderByChild("MenuId").equalTo(catogeryId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot postSnapshot:dataSnapshot.getChildren()){
+                    Food item =postSnapshot.getValue(Food.class);
+                    suggestList.add(item.getName());
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
